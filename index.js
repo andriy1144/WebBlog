e947d1e91ce738593d6ba9d6f16941f4a153c114
Andriy Murhan <andrijmurgan@gmail.com>
2025-03-07 21:49:43 +0200

    Basic validation added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const posts = [
         createdAt: getFormattedDate(), 
         postBody: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting."
     }
-]
+];
 
 function getFormattedDate(){
     return new Date().toLocaleString();
@@ -20,21 +20,23 @@ function getFormattedDate(){
 
 app.get("/", (req,res) =>{
     res.render("index.ejs", {posts: posts});
-})
+});
 
 app.get("/create", (req,res) => {
     res.render("create_page.ejs");
-})
+});
 app.post("/create", (req,res) => {
     const newPost = {
-        title: req.body["post-title"],
+        title: req.body["postTitle"],
         createdAt: getFormattedDate(),
-        postBody: req.body["post-body"]
+        postBody: req.body["postBody"]
     }
 
     posts.push(newPost);
     res.redirect("/");
-})
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
