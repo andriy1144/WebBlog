d2d2576a07697f88bedc44d44e8ce65f6765f39a
Andriy Murhan <andrijmurgan@gmail.com>
2025-03-10 23:33:56 +0200

    Post view page added

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const posts = [
     {
         title: "First Post Title", 
         createdAt: getFormattedDate(), 
-        postBody: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting."
+        postBody: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.",
+        link: `http://localhost:3000/posts/1`
     }
 ];
 
@@ -29,13 +30,22 @@ app.post("/create", (req,res) => {
     const newPost = {
         title: req.body["postTitle"],
         createdAt: getFormattedDate(),
-        postBody: req.body["postBody"]
+        postBody: req.body["postBody"],
+        link: `${req.protocol}://${req.get("host")}/posts/${posts.length + 1}`
     }
 
     posts.push(newPost);
     res.redirect("/");
 });
 
+app.get("/posts/:postId", (req,res) => {
+    const postId = req.params.postId - 1;
+    if(postId < posts.length && postId >= 0){
+        res.render("view_post_page.ejs", {post: posts[postId]});
+    }else{
+        res.render("view_post_page.ejs");
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
