d82998bc215562a8ba97aae109483d6e03cb7b14
Andriy Murhan <andrijmurgan@gmail.com>
2025-03-27 23:03:35 +0200

    Add shity ass icon changing animation :>)

--- public/scripts/generalScripts.js
+++ public/scripts/generalScripts.js
@@ -1,3 +1,14 @@
+const hiddenNav = document.querySelector(".hidden-nav");
+const menuIcon = document.querySelector(".menu-icon");
+
 document.querySelector(".menu-icon").addEventListener("click", () => {
-    document.querySelector(".hidden-nav").classList.toggle("appear-menu");
+    menuIcon.setAttribute("src", "/icons/cross.png");
+    if(hiddenNav.classList.contains("appear-menu")){
+       menuIcon.setAttribute("src", "/icons/menu-sym.svg");
+    }
+    menuIcon.classList.add("menu-icon-animation");
+
+    hiddenNav.classList.toggle("appear-menu");
+
+    setTimeout(() => menuIcon.classList.remove("menu-icon-animation"), 600);
 });
